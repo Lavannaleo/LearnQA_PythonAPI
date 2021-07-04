70ed5bf4f0182a2d82fe528679f9a9161013393d
Lavannaleo <Lavanna.leo@gmail.com>
2021-07-05 02:40:33 +0300

    Ex#10

--- Ex10_ShortPhrase.py
+++ Ex10_ShortPhrase.py
@@ -1,7 +1,7 @@
 class TestShortPhrase:
     def test_short_phrase(self):
         max_lenght = 15
-        phrase = input("Input a short phrase (less 15 symbols): ")
+        phrase = input(f"Input a short phrase (less {max_lenght} symbols): ")
         lenght_phrase = len(phrase)
 
-        assert lenght_phrase <= 15, f"Phrase lenght is more than {max_lenght} symbols"
+        assert lenght_phrase <= max_lenght, f"Phrase lenght is more than {max_lenght} symbols"
