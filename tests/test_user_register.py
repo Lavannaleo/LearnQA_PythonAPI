1d103d3ecbd741dccd6bd757a6210c929a5d0d97
Lavannaleo <Lavanna.leo@gmail.com>
2021-07-14 04:10:48 +0300

    Ex#15

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -17,7 +17,21 @@ class TestUserRegister(BaseCase):
         base_part = "learnqa"
         domain = "example.com"
         random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f"{base_part}{random_part}{domain}"
+        self.email = f"{base_part}{random_part}@{domain}"
+
+    def test_create_user_successfully(self):
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
