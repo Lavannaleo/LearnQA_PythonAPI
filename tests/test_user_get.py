2b40aba31ae59cd27e05d78dada17c27b660f002
Lavannaleo <Lavanna.leo@gmail.com>
2021-07-15 01:53:40 +0300

    Ex#16

---
+++ tests/test_user_get.py
@@ -0,0 +1,76 @@
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from datetime import datetime
+
+class TestUserGet(BaseCase):
+    def setup(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+
+    def test_get_user_details_not_auth(self):
+        response = requests.get("https://playground.learnqa.ru/api/user/2")
+
+        Assertions.assert_json_has_key(response, "username")
+        Assertions.assert_json_has_not_key(response, "email")
+        Assertions.assert_json_has_not_key(response, "firstName")
+        Assertions.assert_json_has_not_key(response, "lastName")
+
+    def test_get_user_details_auth_as_same_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        response2 = requests.get(
+            f"https://playground.learnqa.ru/api/user/{user_id_from_auth_method}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        expected_fields = ["username", "email", "firstName", "lastName"]
+        Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_auth_as_other_user(self):
+        # Create new user
+        data = {
+            'password': '123',
+            'username': 'Leon',
+            'firstName': 'Anna',
+            'lastName': 'leonidova',
+            'email': self.email
+        }
+
+        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+        new_user_id = self.get_json_value(response1, "id")
+
+        # Auth with other user (id=2)
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response2, "user_id")
+
+        # Request info about new user (new_user_id)
+        response3 = requests.get(
+            f"https://playground.learnqa.ru/api/user/{new_user_id}"
+        )
+
+        Assertions.assert_json_has_key(response3, "username")
+        Assertions.assert_json_has_not_key(response3, "email")
+        Assertions.assert_json_has_not_key(response3, "firstName")
+        Assertions.assert_json_has_not_key(response3, "lastName")
